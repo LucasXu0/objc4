f712c810e7cb2e6a6ec42399d072807001b61694
tsuiyuenhong <tsuiyuenhong@gmail.com>
2020-07-15 10:14:30 +0800

    Init Project
    * Add missing headers to external header
    * Replace macosx.internel with macosx
    * Remove <System/pthread_machdep.h
    * Add Preprocessor Macros LIBC_NO_LIBCRASHREPORTERCLIENT
    * Add debuggable macosx target
    * Add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# Objc4
+
+This project is using the latest open-source version of **[objc4-781](https://opensource.apple.com/tarballs/objc4/)**.
+
+
+
+## Installation
+
+1. ``` git clone git@github.com:tsuiyuenhong/objc4.git ```
+2. open `objc4/objc.xcodeproj`
+3. Select `objc_debug_macosx` target, click the build button. Enjoy it!
---
+++ objc_debug_macosx/main.c
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc_debug_macosx
+//
+//  Created by tsuiyuenhong on 2020/7/14.
+//
+
+#include <stdio.h>
+#include <objc/objc-class.h>
+
+int main(int argc, const char * argv[])
+{
+    __auto_type NSObject = objc_getClass("NSObject");
+    __auto_type size = sizeof(*NSObject);
+    printf("size of NSObject = %lu\n", size);
+    return 0;
+}
